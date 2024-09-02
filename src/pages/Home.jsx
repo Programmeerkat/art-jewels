a386fd1b76bd7960a033d36e89a2f9fdb4384dec
unknown <danieltennapel@gmail.com>
2024-09-02 22:33:15 +0200

    added dummy images and mocked homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 function Home() {
 
     return (
-      <>
-        <p>Home</p>
-      </>
+      <div className='overview'>
+        <img src='/images/3FKN03.jpg' alt=""/>
+        <img src='/images/4FKN03.jpg' alt=""/>
+        <img src='/images/7FKN03.jpg' alt=""/>
+        <img src='/images/8FKBA04.jpg' alt=""/>
+        <img src='/images/14FKN07.jpg' alt=""/>
+        <img src='/images/17FKN09.jpg' alt=""/>
+        <img src='/images/48FKBO03.jpg' alt=""/>
+        <img src='/images/48FKBO10.jpg' alt=""/>
+      </div>
     )
   }
   
