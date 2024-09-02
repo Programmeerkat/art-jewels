cbbd012b813cc7ebec6f0c067cac636353cf08af
unknown <danieltennapel@gmail.com>
2024-09-02 22:16:08 +0200

    first draft of responsive header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,29 @@
 import { Link } from "react-router-dom"
 import './Navbar.css'
+import { useState } from "react"
+import logo from './../../assets/icons/logo.svg'
+import menuIcon from './../../assets/icons/menu.svg'
+import closeIcon from './../../assets/icons/close.svg'
 
 function Navbar() {
-
+  const [opened, setOpened] = useState(false)
+  const handleClick = () => setOpened(state => !state)
   return (
     <nav className='navbar'>
-      <ul>
+      <div className="icon">
+        <img src={logo} alt="" width="30" height="30" />
+      </div>
+      <ul className={opened ? 'opened' : ''}>
         <Link to="/">Home</Link>
         <Link to="/bracelets">Bracelets</Link>
         <Link to="/about">About Freddy Knijff</Link>
         <Link to="/contact">Contact</Link>
-      </ul>
+
+      </ul>        
+      <div className="icon hamburger">
+        {!opened && <img onClick={handleClick} src={menuIcon} alt="" width="30" height="30" />}
+        {opened && <img onClick={handleClick} src={closeIcon} alt="" width="30" height="30" />}
+      </div>
     </nav>
   )
 }
