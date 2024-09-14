3199c133e821df9500894cea4decce90857251f7
unknown <danieltennapel@gmail.com>
2024-09-15 00:33:50 +0200

    improved navbar and home overview styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,20 +10,20 @@ function Navbar() {
   const handleClick = () => setOpened(state => !state)
   return (
     <nav className='navbar'>
-      <div className="icon">
-        <img src={logo} alt="" width="30" height="30" />
-      </div>
-      <ul className={opened ? 'opened' : ''}>
-        <Link to="/">Home</Link>
-        <Link to="/bracelets">Bracelets</Link>
-        <Link to="/about">About Freddy Knijff</Link>
-        <Link to="/contact">Contact</Link>
+        <div className="icon">
+          <img src={logo} alt="" width="30" height="30" />
+        </div>
+        <ul className={opened ? 'opened' : ''}>
+          <Link to="/">Home</Link>
+          <Link to="/bracelets">Bracelets</Link>
+          <Link to="/about">About Freddy Knijff</Link>
+          <Link to="/contact">Contact</Link>
 
-      </ul>        
-      <div className="icon hamburger">
-        {!opened && <img onClick={handleClick} src={menuIcon} alt="" width="30" height="30" />}
-        {opened && <img onClick={handleClick} src={closeIcon} alt="" width="30" height="30" />}
-      </div>
+        </ul>        
+        <div className="icon hamburger">
+          {!opened && <img onClick={handleClick} src={menuIcon} alt="" width="30" height="30" />}
+          {opened && <img onClick={handleClick} src={closeIcon} alt="" width="30" height="30" />}
+        </div>
     </nav>
   )
 }
