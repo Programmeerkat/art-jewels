a754240c623def98e8169f7f5858928f490770e9
unknown <danieltennapel@gmail.com>
2024-09-20 19:06:15 +0200

    tweaked layout width

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,12 +11,18 @@ function Navbar() {
   const handleLinkClick = () => setOpened(false);  
   return (
     <nav className='navbar'>
-        <div className="icon">
+      <div className='navbar-container'>
+        <div className="icon logo">
           <img src={logo} alt="" width="30" height="30" />
         </div>
         <ul className={opened ? 'opened' : ''}>
           <Link onClick={handleLinkClick} to="/">Home</Link>
+          <Link onClick={handleLinkClick} to="/rings">Rings</Link>
           <Link onClick={handleLinkClick} to="/bracelets">Bracelets</Link>
+          <Link onClick={handleLinkClick} to="/broches">Broches</Link>
+          <Link onClick={handleLinkClick} to="/necklaces">Necklaces</Link>
+          <Link onClick={handleLinkClick} to="/sculptures">Sculptures</Link>
+          <Link onClick={handleLinkClick} to="/drawings">Drawings</Link>
           <Link onClick={handleLinkClick} to="/about">About Freddy Knijff</Link>
           <Link onClick={handleLinkClick} to="/contact">Contact</Link>
 
@@ -25,6 +31,7 @@ function Navbar() {
           {!opened && <img onClick={handleClick} src={menuIcon} alt="" width="30" height="30" />}
           {opened && <img onClick={handleClick} src={closeIcon} alt="" width="30" height="30" />}
         </div>
+      </div>
     </nav>
   )
 }
